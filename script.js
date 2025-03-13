2c11235aa16561fb9986a32a487b49ddf6eff207
TE23D-Karahodza-Imad <imadahmed.karahodza@elev.ga.ntig.se>
2025-03-13 13:31:22 +0100

    Jag gjorde en kundvagn idag och fixade till några grejer.. också JavaScript fixat lite till.

---
+++ script.js
@@ -0,0 +1,9 @@
+let kundvagnBtn = document.querySelector(".kundvagn_btn");
+
+    
+function ShowCart()
+{
+    document.getElementById("kundvagn").style.display = "flex";
+    console.log("Hello");
+}
+
